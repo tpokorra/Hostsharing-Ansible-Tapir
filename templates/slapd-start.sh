f8ef745b5038a9dc07c4159a4e16c7bbdddab6b6
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2023-01-07 21:14:18 +0100

    fix issues with starting the slapd service from ansible

--- templates/slapd-start.sh
+++ templates/slapd-start.sh
@@ -3,5 +3,5 @@
 export HOME=/home/pacs/{{pac}}/users/{{user}}
 export PID=$HOME/var/run/slapd.pid
 
-/usr/sbin/slapd -h "ldap://127.0.0.1:{{ldap_port}}/" -F "$HOME/slapd2/etc" -d 0  >$HOME/var/log/slapd.log 2>&1 &
+nohup /usr/sbin/slapd -h "ldap://127.0.0.1:{{ldap_port}}/" -F "$HOME/slapd2/etc" -d 0  >>$HOME/var/log/slapd.log 2>&1 &
 echo $! > $PID
